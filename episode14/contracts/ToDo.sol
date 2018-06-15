a59ee9b94b002beb1ef97aec805a0da5e0f16141
Julien Klepatch <julien@julienklepatch.com>
2018-06-15 10:14:28 +0800

    Updated ep14

--- episode14/contracts/ToDo.sol
+++ episode14/contracts/ToDo.sol
@@ -1,21 +1,22 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.24;
 
 contract ToDo {
   string[] tasks;
 
   event Foo(string description);
 
-  //non-constant functions (i.e create a transaction)
+  //Read-only function (i.e will not trigger a transaction)
+  function getTask(uint i) public view returns(string) {
+    return tasks[i];
+  }
+
+  //Write function (i.e Will create a transaction)
   function addTask(string taskName) public {
     tasks.push(taskName);
   }
 
   function triggerEvent(string description) public {
-    Foo(description);
+    emit Foo(description);
   }
 
-  //constant function(s) (i.e will not trigger a transaction)
-  function getTask(uint i) public constant returns(string) {
-    return tasks[i];
-  }
 }
