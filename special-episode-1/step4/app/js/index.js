bb03911eb83ee8ce2134bb356987c01e29e35375
Julien Klepatch <julien@julienklepatch.com>
2018-08-17 15:54:46 +0800

    Fix web3

--- special-episode-1/step4/app/js/index.js
+++ special-episode-1/step4/app/js/index.js
@@ -1,5 +1,6 @@
 console.log('loaded');
 
-web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+web3 = new Web3('http://localhost:8545');
 
 web3.eth.getAccounts(console.log);
+
