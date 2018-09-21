26ff4da1a256c6a24348a0f0c98d21d5c99f5436
Julien Klepatch <julien@julienklepatch.com>
2018-09-21 16:21:55 +0800

    Add todo list dapp step7

---
+++ special-episode-1/step7/app/js/lib/render.js
@@ -0,0 +1,23 @@
+import { formatDate } from './utils';
+
+const renderTasks = ($tasks, tasks = []) => {
+  if(tasks.length == 0) {
+    $tasks.html('<tr><td scope="row">No task created yet...</td></tr>');
+    return;
+  }
+  console.log(tasks);
+  const html = tasks.map((task) => {
+    return `<tr>
+      <td>${task[0]}</td>
+      <td>${formatDate(task[1])}</td>
+      <td>${task[2]}</td>
+      <td>${task[3]}</td>
+      <td>${task[4]}</td>
+     </tr>`;
+   });
+  $tasks.html(html.join(''));
+};
+
+export {
+  renderTasks
+}
---
+++ special-episode-1/step7/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+//Serve static assets (js, css, ...)
+app.use(express.static('app'));
+
+//Serve contract artifact files (ex: ToDo.json)
+app.use(express.static('build/contracts'));
+
+//Serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/app/index.html`);
+});
+
+//If anything else is requested that's an error
+app.get('*', (req, res) => {
+  res.status(404);
+  res.send('Oops... this URL does not exist');
+});
+
+//Start the server
+app.listen(PORT, () =>  {
+  console.log(`ETB Ethereum ToDo List App running on port ${PORT}...`);
+});
