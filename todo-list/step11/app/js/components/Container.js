8fe8c68a7ec237b116f85986e1358a4ea367aedc
Mark Romano <markgromano31@gmail.com>
2018-09-30 17:19:34 -0400

    Step 11 - implement drizzle
    -Replaced ethereum.js with store.js
      -Removed truffle-contract. Drizzle implements contracts under the hood
      -Removed config.js. Drizzle uses provider injected through metamask,
      fallbacks to localhost:8545
    -Refactored App.js to implement render props
      -passes drizzle context from consumer
    -Created Container, Header, Form, and List components
      -Render html but non functional yet

---
+++ todo-list/step11/app/js/components/Container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header'
+import Form from './Form'
+import List from './List'
+
+export default (props) => (
+  <div>
+    <Header/>
+    <Form/>
+    <List/>
+  </div>
+)
