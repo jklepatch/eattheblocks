7a2abf7ed035b369e020165c366c958d5a73f4f7
Julien Klepatch <julien@julienklepatch.com>
2018-12-05 12:09:40 +0800

    Split step10 into 2 steps

--- special-episode-1/step10/app/js/components/App.js
+++ special-episode-1/step10/app/js/components/App.js
@@ -1,6 +1,4 @@
 import React, { Component, Fragment } from 'react';
-import Header from './Header';
-import Footer from './Footer';
 
 class App extends Component {
   constructor(props) {
@@ -13,19 +11,14 @@ class App extends Component {
 
   async componentDidMount(){
     const { web3, todo } = this.props;
-    const accounts = await  web3.eth.getAccounts();
+    const accounts = await web3.eth.getAccounts();
     this.setState({ accounts, address: todo.address });
   }
 
   render() {
     const { accounts, address } = this.state;
     if(accounts.length === 0) return <div>Loading...</div>;
-    return (
-      <Fragment>
-        <Header address={address} />
-        <Footer />
-      </Fragment>
-    );
+    return <div>{accounts[0]}, {address}</div>;
   }
 }
 
