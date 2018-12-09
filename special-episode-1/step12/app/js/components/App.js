a61670fe01770a4f3bf2965a7e0d487b36fa3485
Julien Klepatch <julien@julienklepatch.com>
2018-12-09 12:31:14 +0800

    Re-organized step12

--- special-episode-1/step12/app/js/components/App.js
+++ special-episode-1/step12/app/js/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import Main from './Main';
+import NewTask from './NewTask';
 
 class App extends Component {
   constructor(props) {
@@ -9,21 +9,9 @@ class App extends Component {
     this.state = {
       accounts: [],
       address: null,
-      tasks: [],
+      tasks: []
     };
-    this.getTasks = this.getTasks.bind(this);
     this.createTask = this.createTask.bind(this);
-    this.toggleDone = this.toggleDone.bind(this);
-  }
-
-  async getTasks() {
-    const { todo } = this.props;
-    const taskIds = await todo.methods.getTaskIds().call();
-    const promises = [];
-    taskIds.forEach((taskId) => {
-        promises.push(todo.methods.getTask(taskId).call());
-    });
-    return await Promise.all(promises);
   }
 
   async createTask(content, author) {
@@ -35,47 +23,28 @@ class App extends Component {
         gas: 1000000
       });
     console.log(receipt);
-    const tasks = await this.getTasks();
-    this.setState({tasks});
-  }
-
-  async toggleDone(id) {
-    const { todo } = this.props;
-    const receipt = await todo.methods
-      .toggleDone(id)
-      .send({
-        from: this.state.accounts[0],
-        gas: 1000000
-      });
-    console.log(receipt);
-    const tasks = await this.getTasks();
-    this.setState({tasks});
   }
 
   async componentDidMount(){
     const { web3, todo } = this.props;
     const accounts = await web3.eth.getAccounts();
-    const tasks = await this.getTasks();
-    this.setState({ 
-      accounts,
-      address: todo.options.address, 
-      tasks
-    });
+    this.setState({ accounts, address: todo.options.address });
   }
 
   render() {
-    const { accounts, address, tasks } = this.state;
-
+    const { accounts, address } = this.state;
     if(accounts.length === 0) return <div>Loading...</div>;
     return (
       <Fragment>
         <Header address={address} />
-        <Main 
-          tasks={tasks} 
-          createTask={this.createTask} 
-          toggleDone={this.toggleDone} 
-        />
-        <Footer />
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <NewTask createTask={this.createTask} />
+            </div>
+          </div>
+        </div>
+        <Footer/>
       </Fragment>
     );
   }
