29d2e68c07ee7535c838b0c81d0433800be4c050
Julien Klepatch <julien@julienklepatch.com>
2019-01-14 11:17:20 +0800

    Rename

--- testing/README.md
+++
@@ -1,3 +0,0 @@
-# Testing
-
-Future course on smart contract testing
--- testing/first-test/test/simple-contract.js
+++
@@ -1,7 +0,0 @@
-contract('SimpleContract', () => {
-  it('should deploy', async() => {
-    const contract = await HelloWorld.deployed();
-    const actual = await contract.hello();
-    assert.equal(actual, 'Hello World');
-  });
-});
