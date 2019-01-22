5e87349448d4d0b823ddc098b2cc663e4f77accb
Julien Klepatch <julien@julienklepatch.com>
2019-01-22 15:57:46 +0800

    Reorg day15 DAO

--- smart-contract-30/day15-DAO-partII/DAO.sol
+++ smart-contract-30/day15-DAO-partII/DAO.sol
@@ -2,44 +2,22 @@ pragma solidity ^0.5.2;
 
 /**
  * DAO contract:
- * 1. collects investors money (ether) & allocate shares
- * 2. Allow investors to transfer shares
- * 3. allow investment proposals to be created and voted
- * 4. execute successful investment proposals (i.e send money)
+ * 1. Collects investors money (ether) & allocate shares
+ * 2. Keep track of investor contributions with shares
+ * 3. Allow investors to transfer shares
+ * 4. allow investment proposals to be created and voted
+ * 5. execute successful investment proposals (i.e send money)
  */
 
 contract DAO {
   mapping(address => bool) public investors;
   mapping(address => uint) public shares;
-  mapping(address => mapping(uint => bool)) public votes;
-  struct Proposal {
-    uint id;
-    string name;
-    uint amount;
-    address payable recipient;
-    uint votes;
-    uint end;
-    bool executed;
-  }
-  mapping(uint => Proposal) public proposals;
-  uint public nextProposalId;
+  uint public totalShares;
   uint public availableFunds;
   uint public contributionEnd;
-  uint public totalShares;
-  uint public voteTime;
-  uint public quorum;
-  address public admin;
 
-  constructor(
-    uint contributionTime, 
-    uint _voteTime,
-    uint _quorum) 
-    public {
-    require(_quorum > 0 && _quorum < 100, 'quorum must be between 0 and 100');
+  constructor(uint contributionTime) public {
     contributionEnd = now + contributionTime;
-    voteTime = _voteTime;
-    quorum = _quorum;
-    admin = msg.sender;
   }
 
   function contribute() payable external {
@@ -49,44 +27,19 @@ contract DAO {
     totalShares += msg.value;
     availableFunds += msg.value;
   }
+
+  function redeemShare(uint amount) external {
+    require(shares[msg.sender] >= amount, 'not enough shares');
+    require(availableFunds >= amount, 'not enough available funds');
+    shares[msg.sender] -= amount;
+    availableFunds -= amount;
+    msg.sender.transfer(amount);
+  }
     
   function transferShare(uint amount, address to) external {
     require(shares[msg.sender] >= amount, 'not enough shares');
     shares[msg.sender] -= amount;
     shares[to] += amount;
     investors[to] = true;
-  }
-
-  function createProposal(
-    string memory name,
-    uint amount,
-    address payable recipient) 
-    public 
-    onlyInvestors() {
-    require(availableFunds >= amount, 'amount too big');
-    proposals[nextProposalId] = Proposal(
-      nextProposalId,
-      name,
-      amount,
-      recipient,
-      0,
-      now + voteTime,
-      false
-    );
-    availableFunds -= amount;
-    nextProposalId++;
-  }
-
-  function vote(uint proposalId) external onlyInvestors() {
-    Proposal storage proposal = proposals[proposalId];
-    require(votes[msg.sender][proposalId] == false, 'investor can only vote once for a proposal');
-    require(now < proposal.end, 'can only vote until proposal end date');
-    votes[msg.sender][proposalId] = true;
-    proposal.votes += shares[msg.sender];
-  }
-
-  modifier onlyInvestors() {
-     require(investors[msg.sender] == true, 'only investors');
-     _;
   }
 }
