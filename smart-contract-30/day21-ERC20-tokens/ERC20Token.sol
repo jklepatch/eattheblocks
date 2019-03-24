dad20fac2c164d275fd39f9ef0bd0d306ae8dae4
Julien Klepatch <julien@julienklepatch.com>
2019-03-24 21:16:43 +0800

    Add ERC20 Token

--- smart-contract-30/day21-ERC20-tokens/ERC20Token.sol
+++ smart-contract-30/day21-ERC20-tokens/ERC20Token.sol
@@ -1,76 +1,69 @@
-pragma solidity ^0.5.5;
+pragma solidity ^0.5.6;
 
-contract ERC20Interface {
-    function totalSupply() public view returns (uint256);
-    function transfer(address to, uint256 tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint256 tokens) public returns (bool success);
-    function balanceOf(address tokenOwner) public view returns (uint256 balance);
-    function approve(address spender, uint256 tokens) public returns (bool success);
-    function allowance(address tokenOwner, address spender) public view returns (uint256 remaining);
+interface ERC20Interface {
+    function transfer(address to, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+    function balanceOf(address tokenOwner) public view returns (uint balance);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
+    function totalSupply() public view returns (uint);
 
-    event Transfer(address indexed from, address indexed to, uint256 tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint256 tokens);
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
 
 contract ERC20Token is ERC20Interface {
-
-    uint256 constant private MAX_UINT256 = 2**256 - 1;
-    uint256 public _totalSupply;
-    mapping (address => uint256) public balances;
-    mapping (address => mapping (address => uint256)) public allowed;
-    string public name;           
-    uint8 public decimals; 
+    string public name;
     string public symbol;
-
-
+    uint8 public decimals;
+    uint public totalSupply;
+    mapping(address => uint) public balances;
+    mapping(address => mapping(address => uint)) public allowed;
+    
     constructor(
-        uint256 _initialAmount,
-        string memory _tokenName,
-        uint8 _decimalUnits,
-        string memory _tokenSymbol
-    ) public {
-        balances[msg.sender] = _initialAmount;       
-        _totalSupply = _initialAmount; 
-        name = _tokenName; 
-        decimals = _decimalUnits; 
-        symbol = _tokenSymbol;
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        uint _totalSupply)
+        public {
+            name = _name;
+            symbol = _symbol;
+            decimals = _decimals;
+            totalSupply = _totalSupply;
+            balances[msg.sender] = _totalSupply;
+        }
+        
+    function transfer(address to, uint value) public returns(bool) {
+        require(balances[msg.sender] >= value);
+        balances[msg.sender] -= value;
+        balances[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
     }
     
-    function totalSupply() public view returns(uint256) {
-        return _totalSupply;
-    }
-
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balances[msg.sender] >= _value);
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
+    function transferFrom(address from, address to, uint value) public returns(bool) {
+        uint allowance = allowed[from][msg.sender];
+        require(balances[msg.sender] >= value && allowance >= value);
+        allowed[from][msg.sender] -= value;
+        balances[msg.sender] -= value;
+        balances[to] += value;
+        emit Transfer(msg.sender, to, value);
         return true;
     }
-
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        uint256 allowance = allowed[_from][msg.sender];
-        require(balances[_from] >= _value && allowance >= _value);
-        balances[_to] += _value;
-        balances[_from] -= _value;
-        if (allowance < MAX_UINT256) {
-            allowed[_from][msg.sender] -= _value;
-        }
-        emit Transfer(_from, _to, _value);
+    
+    function approve(address spender, uint value) public returns(bool) {
+        require(spender != msg.sender);
+        allowed[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
         return true;
     }
-
-    function balanceOf(address _owner) public view returns (uint256 balance) {
-        return balances[_owner];
+    
+    function allowance(address owner, address spender) public view returns(uint) {
+        return allowed[owner][spender];
     }
-
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
+    
+    function balanceOf(address owner) public view returns(uint) {
+        return balances[owner];
     }
-
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
-        return allowed[_owner][_spender];
-    }
+    
 }
