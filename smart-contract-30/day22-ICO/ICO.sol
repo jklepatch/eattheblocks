dfc9bf37847819dd877b92967d3d3822f00aa9f1
Julien Klepatch <julien@julienklepatch.com>
2019-04-16 16:28:26 +0800

    add few episodes smartcontract30

--- smart-contract-30/day22-ICO/ICO.sol
+++ smart-contract-30/day22-ICO/ICO.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.6;
+pragma solidity ^0.5.7;
 
 interface ERC20Interface {
     function transfer(address to, uint tokens) external returns (bool success);
@@ -68,130 +68,133 @@ contract ERC20Token is ERC20Interface {
 }
 
 contract ICO {
-  struct Sale {
-    address investor;
-    uint quantity; //quantity of token sold
-  }
-  address public token;
-  uint public price; //token per wei
-  uint public availableTokens; //# of tokens available for sale
-  uint public minPurchase;
-  uint public maxPurchase;
-  uint public end;
-  bool public released;
-  address public admin;
-  mapping(address => bool) public investors; //pre-approved investors
-  Sale[] public sales;
-
-  constructor(
-    string memory _name,
-    string memory _symbol,
-    uint8 _decimals,
-    uint _totalSupply,
-    uint _price) public {
-    token = address(new ERC20Token(
-      _name,
-      _symbol,
-      _decimals,
-      _totalSupply
-    ));
-    admin = msg.sender;
-  }
-
-  function start(
-    uint duration, 
-    uint _price, 
-    uint _availableTokens,
-    uint _minPurchase,
-    uint _maxPurchase) 
-    external 
-    onlyAdmin() 
-    icoActive(false) {
-    uint totalSupply = ERC20Token(token).totalSupply();
-    require(duration > 0, 'duration needs to be > 0');
-    require(_availableTokens > 0 && _availableTokens <= totalSupply, '_availableTokens need be above 0 and max equal to token totalSupply');
-    require(_minPurchase > 0, '_minPurchase needs to be > 0'); 
-    require(_maxPurchase > 0 && _maxPurchase <= _availableTokens , '_maxPurchase needs to be > 0'); 
-    end = now + duration;
-    price = _price;
-    availableTokens = _availableTokens;
-    minPurchase = _minPurchase;
-    maxPurchase = _maxPurchase;
-  }
-
-  function whitelist(address investor) 
-    external 
-    onlyAdmin() {
-    investors[investor] = true;
-  }
-
-  function buy() 
-    payable 
-    external 
-    onlyInvestors()
-    icoActive(true) {
-    uint quantity = msg.value * price;
-    require(msg.value >= minPurchase && msg.value <= maxPurchase, 'have to send between minPurchase and maxPurchase');
-    require(quantity <= availableTokens, 'Not enough tokens left for sale');
-    require(msg.value % price == 0, 'have to send a multiple of price');
-    availableTokens += quantity;
-    sales.push(Sale(msg.sender, quantity));
-  }
-
-  function release()
-    external 
-    onlyAdmin() 
-    icoEnded() 
-    tokensReleased(false) {
-    ERC20Token tokenInstance = ERC20Token(token);
-    for(uint i = 0; i < sales.length; i++) {
-      Sale storage sale = sales[i];
-      tokenInstance.transfer(sale.investor, sale.quantity);
+    struct Sale {
+        address investor;
+        uint quantity;
     }
-  }
-
-  function withdraw(
-    address payable to,
-    uint amount) 
-    external
-    onlyAdmin()
-    icoEnded()
-    tokensReleased(true) {
-    address(to).transfer(amount);
-  }
-  
-  modifier onlyAdmin() {
-    require(msg.sender == admin, 'only admin');
-    _;
-  }
-
-  modifier onlyInvestors() {
-    require(investors[msg.sender] == true, 'only investors');
-    _;
-  }
-
-  modifier icoActive(bool activeStatus) {
-    bool status = end > 0 && now < end && availableTokens > 0;
-    if(activeStatus == true) {
-      require(status == true, 'ICO must be active');
-    } else {
-      require(status == false, 'ICO must NOT be active');
+    Sale[] public sales;
+    mapping(address => bool) public investors;
+    address public token;
+    address public admin;
+    uint public end;
+    uint public price;
+    uint public availableTokens;
+    uint public minPurchase;
+    uint public maxPurchase;
+    bool public released;
+    
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        uint _totalSupply)
+        public {
+        token = address(new ERC20Token(
+            _name,
+            _symbol,
+            _decimals,
+            _totalSupply
+        ));
+        admin = msg.sender;
     }
-    _;
-  }
-
-  modifier icoEnded() {
-    require(end > 0 
-      && (now >= end || availableTokens == 0), 'ICO must have ended');
-    _;
-  }
-
-  modifier tokensReleased(bool releasedStatus) {
-    if(releasedStatus == true) {
-      require(released == true, 'Token must have been released');
-    } else {
-      require(released == false, 'Token must NOT have been released');
+    
+    function start(
+        uint duration,
+        uint _price,
+        uint _availableTokens,
+        uint _minPurchase,
+        uint _maxPurchase)
+        external
+        onlyAdmin() 
+        icoNotActive() {
+        require(duration > 0, 'duration should be > 0');
+        uint totalSupply = ERC20Token(token).totalSupply();
+        require(_availableTokens > 0 && _availableTokens <= totalSupply, 'totalSupply should be > 0 and <= totalSupply');
+        require(_minPurchase > 0, '_minPurchase should > 0');
+        require(_maxPurchase > 0 && _maxPurchase <= _availableTokens, '_maxPurchase should be > 0 and <= _availableTokens');
+        end = duration + now; 
+        price = _price;
+        availableTokens = _availableTokens;
+        minPurchase = _minPurchase;
+        maxPurchase = _maxPurchase;
     }
-    _;
-  }
+    
+    function whitelist(address investor)
+        external
+        onlyAdmin() {
+        investors[investor] = true;    
+    }
+    
+    function buy()
+        payable
+        external
+        onlyInvestors()
+        icoActive() {
+        require(msg.value % price == 0, 'have to send a multiple of price');
+        require(msg.value >= minPurchase && msg.value <= maxPurchase, 'have to send between minPurchase and maxPurchase');
+        uint quantity = price * msg.value;
+        require(quantity <= availableTokens, 'Not enough tokens left for sale');
+        sales.push(Sale(
+            msg.sender,
+            quantity
+        ));
+    }
+    
+    function release()
+        external
+        onlyAdmin()
+        icoEnded()
+        tokensNotReleased() {
+        ERC20Token tokenInstance = ERC20Token(token);
+        for(uint i = 0; i < sales.length; i++) {
+            Sale storage sale = sales[i];
+            tokenInstance.transfer(sale.investor, sale.quantity);
+        }
+    }
+    
+    function withdraw(
+        address payable to,
+        uint amount)
+        external
+        onlyAdmin()
+        icoEnded()
+        tokensReleased() {
+        to.transfer(amount);    
+    }
+    
+    modifier icoActive() {
+        require(end > 0 && now < end && availableTokens > 0, "ICO must be active");
+        _;
+    }
+    
+    modifier icoNotActive() {
+        require(end == 0, 'ICO should not be active');
+        _;
+    }
+    
+    modifier icoEnded() {
+        require(end > 0 && (now >= end || availableTokens == 0), 'ICO must have ended');
+        _;
+    }
+    
+    modifier tokensNotReleased() {
+        require(released == false, 'Tokens must NOT have been released');
+        _;
+    }
+    
+    modifier tokensReleased() {
+        require(released == true, 'Tokens must have been released');
+        _;
+    }
+    
+    modifier onlyInvestors() {
+        require(investors[msg.sender] == true, 'only investors');
+        _;
+    }
+    
+    modifier onlyAdmin() {
+        require(msg.sender == admin, 'only admin');
+        _;
+    }
+    
 }
