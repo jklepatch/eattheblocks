731677eb8a7db71f4d5a81fcfe0975848ecbfab0
Julien Klepatch <julien@julienklepatch.com>
2019-05-15 01:23:41 +0800

    Add tweeter

--- smart-contract-30/day25-tweeter/Tweeter.sol
+++ smart-contract-30/day25-tweeter/Tweeter.sol
@@ -1,147 +1,139 @@
-pragma solidity ^0.5.7;
+pragma solidity ^0.5.8;
 pragma experimental ABIEncoderV2;
 
-contract Tweeter {
-  struct Tweet {
-    uint id;
-    address author;
-    string content;
-    uint createdAt;
-  }
-
-  struct Message {
-    address author;
-    string content;
-    uint createdAt;
-  }
-
-  mapping(uint => Tweet) private tweets;
-  mapping(address => uint[]) private tweetsOf;
-  mapping(address => address[]) private following;
-  mapping(address => Message[]) private conversations;
-  mapping(address => mapping(address => bool)) private operators;
-  uint private nextId;
-  address public admin;
-
-  event TweetSent(
-    uint id,
-    address indexed _from,
-    string content,
-    uint date
-  );
-  event FollowerAdded(
-    address indexed _from, 
-    address indexed _followed, 
-    uint indexed date
-  );
-  event MessageSent(
-    address indexed _from, 
-    address indexed _to, 
-    string content
-  );
-
-  constructor(address _admin) public {
-    admin = _admin;
-  }
-
-  function tweet(string calldata _content) external {
-    _tweet(_content, msg.sender);
-  }
-
-  function tweetFrom(string calldata _content, address _from) external {
-    _tweet(_content, _from);
-  }
-
-  function tweetTo(address _to, string calldata _content) external {
-    _tweetTo(_to, _content, msg.sender);
-  }
-
-  function tweetToFrom(
-    address _to, 
-    string calldata _content,
-    address _from) 
-    external {
-    _tweetTo(_to, _content, _from);
-  }
-
-  function follow(address _followed) external {
-    _follow(_followed, msg.sender);
-  }
-
-  function followFrom(address _followed, address _from) external {
-    _follow(_followed, _from);
-  }
-
-  function getTweetsOf(address author) 
-    external 
-    view 
-    returns(Tweet[] memory) {
-    uint[] storage tweetIds = tweetsOf[author];
-    Tweet[] memory _tweets = new Tweet[](tweetIds.length);
-    for(uint i = 0; i < tweetIds.length; i++) {
-      Tweet storage _tweet = tweets[i];
-      _tweets[i] = Tweet(
-        _tweet.id, 
-        _tweet.author, 
-        _tweet.content, 
-        _tweet.createdAt
-      );
+//1. Send tweet
+//2. Send private messages
+//3. Follow other people
+//4. Get list of tweets
+//5. Implement an API
+contract Twitter {
+    struct Tweet {
+        uint id;
+        address author;
+        string content;
+        uint createdAt;
     }
-    return tweets;
-  }
-
-  function getLatestTweets(uint count) 
-    external 
-    view 
-    returns(Tweet[] memory) {
-    require(count > 0 && count <= nextId, 'Not enough tweets');
-    Tweet[] memory _tweets = new Tweet[](nextId);
-    for(uint i = nextId - count; i < nextId; i++) {
-      Tweet storage _tweet = tweets[i];
-      _tweets[i] = Tweet(
-        _tweet.id, 
-        _tweet.author, 
-        _tweet.content, 
-        _tweet.createdAt
-      );
+    
+    struct Message {
+        uint id;
+        string content;
+        address from;
+        address to;
+        uint createdAt;
     }
-    return _tweets;
-  } 
-
-  function _tweet(
-    string memory _content,
-    address _from)
-    internal 
-    canOperate(_from) {
-    tweets[nextId] = Tweet(nextId, _from, _content, now);
-    tweetsOf[_from].push(nextId);
-    emit TweetSent(nextId, msg.sender, _content, now);    
-    nextId++;
-  }
-
-  function _tweetTo(
-    address _to, 
-    string memory _content, 
-    address _from) 
-    internal 
-    canOperate(_from) {
-    uint id = uint(_from) + uint(_to);
-    conversations[id].push(Message(_from, _content, now));
-    emit MessageSent(_from, _to, _content);
-  }
-
-  function _follow(
-    address _followed, 
-    address _from) 
-    internal 
-    canOperate(_from) {
-    following[_to].push(_followed);
-    emit FollowerAdded(_from, _followed, now);
-  }
-
-  modifier canOperate(address _from) {
-    require(msg.sender == _from 
-            || operators[_from][msg.sender] == true, 'Cannot operate this account');
-    _;
-  }
+    mapping(uint => Tweet) private tweets;
+    mapping(address => uint[]) private tweetsOf;
+    mapping(uint => Message[]) private conversations;
+    mapping(address => address[]) private following;
+    mapping(address => mapping(address => bool)) private operators;
+    uint private nextTweetId;
+    uint private nextMessageId;
+    
+    event TweetSent (
+        uint id,
+        address indexed author,
+        string content,
+        uint createdAt
+    );
+    
+    event MessageSent(
+        uint id,
+        string content,
+        address indexed from,
+        address indexed to,
+        uint createdAt
+    );
+    
+    function tweet(string calldata _content) external {
+        _tweet(msg.sender, _content);
+    }
+    
+    function tweetFrom(address _from, string calldata _content) external {
+        _tweet(_from, _content);
+    }
+    
+    function sendMessage(
+        string calldata _content,
+        address _to)
+        external {
+         _sendMessage(_content, msg.sender, _to);  
+    }
+    
+    function sendMessageFrom(
+        string calldata _content,
+        address _from,
+        address _to)
+        external {
+        _sendMessage(_content, _from, _to);    
+    }
+    
+    function follow(address _followed) external {
+        following[msg.sender].push(_followed);
+    }
+    
+    function getLatestTweets(uint count) view external returns(Tweet[] memory) {
+        require(count > 0 && count <= nextTweetId, 'Too few or too many tweets to return');
+        Tweet[] memory _tweets = new Tweet[](count);
+        for(uint i = nextTweetId - count; i < nextTweetId; i++) {
+            Tweet storage _tweet = tweets[i];
+            _tweets[i] = Tweet(
+                _tweet.id, 
+                _tweet.author, 
+                _tweet.content, 
+                _tweet.createdAt
+            );
+        }
+        return _tweets;
+    }
+    
+    function getTweetsOf(address _user, uint count) view external returns(Tweet[] memory) {
+        uint[] storage tweetIds = tweetsOf[_user];
+        require(count > 0 && count <= tweetIds.length, 'Too few or too many tweets to return');
+        Tweet[] memory _tweets = new Tweet[](count);
+        for(uint i = tweetIds.length - count; i < tweetIds.length; i++) {
+            Tweet storage _tweet = tweets[tweetIds[i]];
+            _tweets[i] = Tweet(
+                _tweet.id, 
+                _tweet.author, 
+                _tweet.content, 
+                _tweet.createdAt
+            );
+        }
+        return _tweets;
+    }
+    
+    function _tweet(
+        address _from, 
+        string memory _content) 
+        internal
+        canOperate(_from) {
+        tweets[nextTweetId] = Tweet(nextTweetId, _from, _content, now);
+        tweetsOf[_from].push(nextTweetId);
+        emit TweetSent(nextTweetId, _from, _content, now);
+        nextTweetId++;
+    }
+        
+    function _sendMessage(
+        string memory _content,
+        address _from,
+        address _to)
+        internal
+        canOperate(_from) {
+        uint conversationId = uint(_from) + uint(_to);
+        conversations[conversationId].push(Message(
+            nextMessageId, 
+            _content, 
+            _from,
+            _to, 
+            now)
+        );
+        emit MessageSent(nextMessageId, _content, _from, _to, now);
+        nextMessageId++;  
+    }
+    
+    modifier canOperate(address _from) {
+        require(operators[_from][msg.sender] == true, 'Operator not authorized');
+        _;
+    }
+    
 }
