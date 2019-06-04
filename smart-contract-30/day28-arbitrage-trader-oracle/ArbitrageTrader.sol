bd34a94872bd552bfe731b11ff0871fd77b76d37
Julien Klepatch <julien@julienklepatch.com>
2019-06-04 22:43:22 +0800

    Added day28 on sc30

--- smart-contract-30/day28-arbitrage-trader-oracle/ArbitrageTrader.sol
+++ smart-contract-30/day28-arbitrage-trader-oracle/ArbitrageTrader.sol
@@ -1,60 +1,58 @@
 pragma solidity ^0.5.8;
 pragma experimental ABIEncoderV2;
 
-import 'browser/Oracle.sol';
 import 'browser/Dex.sol';
+import 'browser/Oracle.sol';
 
 contract ArbitrageTrader {
     struct Asset {
-        string sticker;
+        string name;
         address dex;
     }
     mapping(string => Asset) public assets;
     address public admin;
     address public oracle;
     
-    constructor () public {
+    constructor() public {
         admin = msg.sender;
     }
     
-    function configureOracle(
-        address _oracle) 
-        external
-        onlyAdmin() {
-        oracle = _oracle;     
+    function configureOracle(address _oracle) external onlyAdmin() {
+        oracle = _oracle;
     }
     
-    function configureAssets(Asset[] calldata _assets) 
-        external
-        onlyAdmin() {
+    function configureAssets(Asset[] calldata _assets) external onlyAdmin() {
         for(uint i = 0; i < _assets.length; i++) {
-            assets[_assets[i].sticker] = Asset(_assets[i].sticker, _assets[i].dex);
+            assets[_assets[i].name] = Asset(_assets[i].name, _assets[i].dex);
         }
     }
     
     function maybeTrade(
-        string calldata _sticker, 
-        uint _date) 
+        string calldata _sticker,
+        uint _date)
         external
         onlyAdmin() {
         Asset storage asset = assets[_sticker];
         require(asset.dex != address(0), 'This asset does not exist');
         
+        // Get latest price of asset from oracle
         bytes32 dataKey = keccak256(abi.encodePacked(_sticker, _date));
         Oracle oracleContract = Oracle(oracle);
         Oracle.Result memory result = oracleContract.getData(dataKey);
-        require(result.exist == true, 'This result does not exist, cant trade');
-        require(result.approvedBy.length == 10, 'Not enough approvals for this data, cant trade');
+        require(result.exist == true, 'This result does not exist, cannot trade');
+        require(result.approvedBy.length == 10, 'Not enough approvals for this result, cannot trade');
         
+        //If there is a price, trade of the dex
         Dex dexContract = Dex(asset.dex);
         uint price = dexContract.getPrice(_sticker);
+        uint amount = 1 ether / price;
         if(price > result.payload) {
-            dexContract.sell(_sticker, 100 , (99 * price) / 100); // fix amount
-        } else {
-            dexContract.buy(_sticker, 100, (101 * price) / 100); //fix amount
+            dexContract.sell(_sticker, amount, (99 * price) / 100);
+        } else if(price < result.payload) {
+            dexContract.buy(_sticker, amount, (101 * price) / 100);
         }
     }
-
+    
     modifier onlyAdmin() {
         require(msg.sender == admin, 'Only admin');
         _;
--- smart-contract-30/day28-arbitrage-trader-oracle/Dex.sol
+++ smart-contract-30/day28-arbitrage-trader-oracle/Dex.sol
@@ -1,17 +1,16 @@
 pragma solidity ^0.5.8;
 
 contract Dex {
-    mapping(string => uint) public prices;
-    
-    function getPrice(string calldata _sticker) view external returns(uint) {
+    mapping(string => uint) private prices;
+    function getPrice(string calldata _sticker) external view returns(uint) {
         return prices[_sticker];
     }
     
     function buy(string calldata _sticker, uint _amount, uint _price) external {
-        //buy logic here
+        //Buy ERC20 token
     }
     
     function sell(string calldata _sticker, uint _amount, uint _price) external {
-        //sell logic here
+        //Sell ERC20 token
     }
 }
