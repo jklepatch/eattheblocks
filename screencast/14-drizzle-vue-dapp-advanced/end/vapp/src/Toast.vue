0e6f355a8d13a1a6ae6fbdcf5876d01c9e574134
Julien Klepatch <julien@julienklepatch.com>
2019-07-10 07:17:42 -0400

    Update drizzle-vue-plugin

--- screencast/14-drizzle-vue-dapp-advanced/end/vapp/src/Toast.vue
+++ screencast/14-drizzle-vue-dapp-advanced/end/vapp/src/Toast.vue
@@ -4,21 +4,20 @@
 
 <script>
 import Toasted from 'vue-toasted';
-import { DrizzleEvents } from '@drizzle/vue-plugin';
 import Vue from 'vue';
+
 Vue.use(Toasted);
 
 export default {
   mounted() {
-    // See docs: https://github.com/shakee93/vue-toasted#options
-    const subOptions = { duration: 3000 } // 3 seconds
     const contractEventHandler = ({ contractName, eventName, data }) => {
-      const display = `${contractName}(${eventName}): storedData updated to '${data.newValue}' on ${new Date(parseInt(data.date))}`;
+      const display = `${contractName}(${eventName}): ${data.newValue}, ${data.date}`;
+      const subOptions = { duration: 3000 };
       this.$toasted.show(display, subOptions);
-    }
-    DrizzleEvents.$on('drizzle/contractEvent', payload => {
-      contractEventHandler(payload)
-    })
+    };
+    this.$drizzleEvents.$on('drizzle/contractEvent', payload => {
+      contractEventHandler(payload);
+    });
   }
 }
 </script>
