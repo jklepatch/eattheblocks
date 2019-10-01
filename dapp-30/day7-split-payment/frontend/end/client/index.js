e56c9d64ee56cc2c4d9da961044bee7a039775b0
Julien Klepatch <julien@julienklepatch.com>
2019-10-01 12:08:56 +0800

    add day7

--- dapp-30/day7-split-payment/frontend/end/client/index.js
+++ dapp-30/day7-split-payment/frontend/end/client/index.js
@@ -1,14 +1,44 @@
-import SplitPayment from '../build/contracts/SplitPayment.json';
 import Web3 from 'web3';
+import SplitPayment from '../build/contracts/SplitPayment.json';
 
-const web3 = new Web3('http://localhost:9545');
-const deploymentKey = Object.keys(SplitPayment.networks)[0];
-const splitPayment = new web3.eth.Contract(
-  SplitPayment.abi, 
-  SplitPayment.networks[deploymentKey].address
-);
+let web3;
+let splitPayment;
 
-document.addEventListener('DOMContentLoaded', () => {
+const initWeb3 = () => {
+  return new Promise((resolve, reject) => {
+    if(typeof window.ethereum !== 'undefined') {
+      const web3 = new Web3(window.ethereum);
+      window.ethereum.enable()
+        .then(() => {
+          resolve(
+            new Web3(window.ethereum)
+          );
+        })
+        .catch(e => {
+          reject(e);
+        });
+      return;
+    }
+    if(typeof window.web3 !== 'undefined') {
+      return resolve(
+        new Web3(window.web3.currentProvider)
+      );
+    }
+    resolve(new Web3('http://localhost:9545'));
+  });
+};
+
+const initContract = async () => {
+  const networkId = await web3.eth.net.getId();
+  return new web3.eth.Contract(
+    SplitPayment.abi, 
+    SplitPayment
+      .networks[networkId]
+      .address
+  );
+};
+
+const initApp = () => {
   const $send = document.getElementById('send');
   const $sendResult = document.getElementById('send-result');
   let accounts = [];
@@ -35,5 +65,18 @@ document.addEventListener('DOMContentLoaded', () => {
         $sendResult.innerHTML = `Ooops... there was an error while trying to send a split payment...`;
       });
   });
+};
 
+
+document.addEventListener('DOMContentLoaded', () => {
+  initWeb3()
+    .then(_web3 => {
+      web3 = _web3;
+      return initContract();
+    })
+    .then(_splitPayment => {
+      splitPayment = _splitPayment;
+      initApp(); 
+    })
+    .catch(e => console.log(e.message));
 });
