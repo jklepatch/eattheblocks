983ac1364e28d1236f1d765aadc3ad16563c5f2d
Julien Klepatch <julien@julienklepatch.com>
2019-10-14 00:44:09 +0800

    Add frontend for dapp30/day12

--- dapp-30/day12-fibonacci/frontend/end/client/src/App.js
+++ dapp-30/day12-fibonacci/frontend/end/client/src/App.js
@@ -1,72 +1,60 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Fibonacci from './contracts/Fibonacci.json';
 import { getWeb3 } from './utils.js';
 
-class App extends Component {
-  state = {
-    web3: null,
-    accounts: [],
-    contract: null,
-    result: null
-  }
+function App() {
+  const [web3, setWeb3] = useState(undefined);
+  const [contract, setContract] = useState(undefined);
+  const [result, setResult] = useState(undefined);
 
-  componentDidMount = async () => {
-    const web3 = await getWeb3();
-    const accounts = await web3.eth.getAccounts();
+  useEffect(() => {
+    const init = async () => {
+      const web3 = await getWeb3();
 
-    setInterval(async () => {
-      const accounts = await web3.eth.getAccounts()
-      if (accounts[0] !== this.state.accounts[0]) {
-        this.setState({accounts});
-      }
-    }, 1000);
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = Fibonacci.networks[networkId];
+      const contract = new web3.eth.Contract(
+        Fibonacci.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+      setWeb3(web3);
+      setContract(contract);
+    }
+    init();
+  }, []);
 
-    const networkId = await web3.eth.net.getId();
-    const deployedNetwork = Fibonacci.networks[networkId];
-    const contract = new web3.eth.Contract(
-      Fibonacci.abi,
-      deployedNetwork && deployedNetwork.address,
-    );
 
-    this.setState({ web3, accounts, contract });
-  };
-
-  calculate = async e => {
+  async function calculate(e) {
     e.preventDefault();
-    const { contract } = this.state;
     const result = await contract.methods
       .fib(e.target.elements[0].value)
       .call();
-    this.setState({result});
+    setResult(result);
   }
 
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading...</div>;
-    }
+  if (!web3) {
+    return <div>Loading...</div>;
+  }
 
-    const { result } = this.state;
+  return (
+    <div className="container">
+      <h1 className="text-center">Fibonacci</h1>
 
-    return (
-      <div className="container">
-        <h1 className="text-center">Fibonacci</h1>
-
-        <div className="row">
-          <div className="col-sm-12">
-            <form onSubmit={e => this.calculate(e)}>
-              <div className="form-group">
-                <label htmlFor="number">Fibonacci sequence of</label>
-                <input type="number" className="form-control" id="number" />
-              </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
-              <p>{result && `Result: ${result}`}</p>
-            </form>
-          </div>
+      <div className="row">
+        <div className="col-sm-12">
+          <form onSubmit={e => calculate(e)}>
+            <div className="form-group">
+              <label htmlFor="number">Fibonacci sequence of</label>
+              <input type="number" className="form-control" id="number" />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+            <p>{result && `Result: ${result}`}</p>
+          </form>
         </div>
-
       </div>
-    );
-  }
+
+    </div>
+  );
 }
 
 export default App;
