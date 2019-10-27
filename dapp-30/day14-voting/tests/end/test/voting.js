7d128c0c55e312224298f4d3f0b0f5564d03bdfd
Julien Klepatch <julien@julienklepatch.com>
2019-10-28 01:40:36 +0800

    add tests for day14

--- dapp-30/day14-voting/tests/end/test/voting.js
+++ dapp-30/day14-voting/tests/end/test/voting.js
@@ -13,91 +13,79 @@ contract('Voting', (accounts) => {
   });
 
   it('should add voters', async () => {
-    await voting.addVoters([voter1, voter2, voter3], {from: admin});
-    const result1 = await voting.voters(voter1);
-    const result2 = await voting.voters(voter2);
-    const result3 = await voting.voters(voter3);
-    assert(result1 === true);
-    assert(result2 === true);
-    assert(result3 === true);
+    await voting.addVoters([voter1, voter2, voter3]);
+    const results = await Promise.all(
+      [voter1, voter2, voter3].map(voter =>
+        voting.voters(voter)
+      )
+    );
+    results.forEach(result => assert(result === true));
   });
 
-  it('should create ballot', async () => {
+  it('should create a new ballot', async () => {
     await voting.createBallot(
-      'ballot1',
-      ['choice1', 'choice2', 'choice3'], 
-      5, 
+      'Ballot 1',
+      ['Choice 1', 'Choice 2', 'Choice 3'],
+      5,
       {from: admin}
     );
-    //ballots mapping is not public, we cant check that ballot was actually created :(
+    const ballot = await voting.ballots(0);
+    assert(ballot.name === 'Ballot 1');
   });
 
-  it('should NOT let non-admin create ballot', async () => {
+  it('should NOT create a new ballot if not admin', async () => {
     await expectRevert(
       voting.createBallot(
-        'Ballot2',
-        ['choice1', 'choice2', 'choice3'], 
-        10, 
-        {from: nonVoter}
+        'Ballot 2',
+        ['Choice 1', 'Choice 2', 'Choice 3'],
+        5,
+        {from: voter1}
       ),
       'only admin'
     );
   });
 
-  it('should NOT let non-voters vote', async () => {
+  it('should NOT vote if not a voter', async () => {
     await voting.createBallot(
-      'Ballot2',
-      ['choice1', 'choice2', 'choice3'], 
-      10, 
+      'Ballot 3',
+      ['Choice 1', 'Choice 2', 'Choice 3'],
+      5,
       {from: admin}
     );
     await expectRevert(
-      voting.vote(1, 1, {from: nonVoter}),
+      voting.vote(1, 0, {from: nonVoter}),
       'only voters can vote'
     );
   });
 
-  it('should NOT let voters vote after end of ballot', async () => {
+  it('should NOT vote after ballot end', async () => {
     await voting.createBallot(
-      'Ballot3',
-      ['choice1', 'choice2', 'choice3'], 
-      1, 
+      'Ballot 4',
+      ['Choice 1', 'Choice 2', 'Choice 3'],
+      5,
       {from: admin}
     );
-    await time.increase(1001);
+    await time.increase(5001);
     await expectRevert(
-     voting.vote(2, 1, {from: voter1}),
+      voting.vote(2, 0, {from: voter1}),
       'can only vote until ballot end date'
-    );
-  });
-
-  it('should NOT let voters vote twice', async () => {
-    await voting.createBallot(
-      'Ballot4',
-      ['choice1', 'choice2', 'choice3'], 
-      2, 
-      {from: admin}
-    );
-    await voting.vote(3, 1, {from: voter1});
-    await expectRevert(
-      voting.vote(3, 1, {from: voter1}),
-      'voter can only vote once for a ballot'
     );
   });
 
   it('should vote', async () => {
     await voting.createBallot(
-      'Ballot5',
-      ['choice1', 'choice2', 'choice3'], 
-      2, 
+      'Ballot 5',
+      ['Choice 1', 'Choice 2', 'Choice 3'],
+      5,
       {from: admin}
     );
-    await voting.vote(4, 1, {from: voter1});
-    await voting.vote(4, 1, {from: voter2});
-    await voting.vote(4, 2, {from: voter3});
-    await time.increase(2001);
-    const result = await voting.results(4);
-    assert(result[1].votes === '2');
-    assert(result[2].votes === '1');
+    await voting.vote(3, 0, {from: voter1}),
+    await voting.vote(3, 0, {from: voter2}),
+    await voting.vote(3, 1, {from: voter3}),
+    await time.increase(5001);
+    const results = await voting.results(3); 
+    assert(results[0].votes === '2');
+    assert(results[1].votes === '1');
+    assert(results[2].votes === '0');
   });
 });
