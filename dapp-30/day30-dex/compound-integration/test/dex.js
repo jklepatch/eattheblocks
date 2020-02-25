6d8bbc9e408549a9c9870fbf40c1c007680ef9ec
Julien Klepatch <julien@julienklepatch.com>
2020-02-25 18:32:56 +0900

    add compound-integration

---
+++ dapp-30/day30-dex/compound-integration/test/dex.js
@@ -0,0 +1,29 @@
+const Dai = artifacts.require('mocks/Dai.sol');
+const Bat = artifacts.require('mocks/Bat.sol');
+const Rep = artifacts.require('mocks/Rep.sol');
+const Zrx = artifacts.require('mocks/Zrx.sol');
+const Dex = artifacts.require('Dex.sol');
+
+contract('Dex', (accounts) => {
+  let dex, dai, bat, rep, zrx;
+  const [DAI, BAT, REP, ZRX] = ['DAI', 'BAT', 'REP', 'ZRX']
+    .map(ticker => web3.utils.fromAscii(ticker));
+  beforeEach(async() => {
+    ([dai, bat, rep, zrx] = await Promise.all([
+      Dai.new(), 
+      Bat.new(), 
+      Rep.new(), 
+      Zrx.new()
+    ]));
+    dex = await Dex.new();
+    await Promise.all([
+      dex.addToken(DAI, dai.address, accounts[4]),
+      dex.addToken(BAT, bat.address, accounts[5]),
+      dex.addToken(REP, rep.address, accounts[6]),
+      dex.addToken(ZRX, zrx.address, accounts[7])
+    ]);
+  });
+
+  it('should work!', () => {
+  });
+});
