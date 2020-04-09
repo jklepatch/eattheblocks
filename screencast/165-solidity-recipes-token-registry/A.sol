ea60ae94ca62cc6ab7a1ff0afe94fd09ac4c73b2
Julien Klepatch <julien@julienklepatch.com>
2020-04-09 10:54:45 +0800

    add token registry

---
+++ screencast/165-solidity-recipes-token-registry/A.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.6.0;
+
+import "./Token.sol";
+import "./Registry.sol";
+
+contract A {
+    Registry registry;
+    address admin;
+    
+    constructor() public {
+        admin = msg.sender;
+    }
+    
+    function updateRegistry(address registryAddress) external {
+        require(msg.sender == admin);
+        registry  = Registry(registryAddress);
+    }
+
+    function foo() external {
+        Token token = Token(registry.tokens("ABC"));
+        token.transfer(100, msg.sender);
+    }
+}
---
+++ screencast/165-solidity-recipes-token-registry/Token.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.6.0;
+
+contract Token {
+    function transfer(uint amount, address to) external {
+        //transfer tokens...
+    }
+    //etc...
+}
