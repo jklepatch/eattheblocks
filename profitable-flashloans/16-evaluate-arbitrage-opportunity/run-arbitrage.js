69067cfbff07c644709c10ecf463e57ce6c2cdfc
Julien Klepatch <julien@julienklepatch.com>
2020-06-21 15:14:23 +0800

    add new step flashloan

---
+++ profitable-flashloans/16-evaluate-arbitrage-opportunity/run-arbitrage.js
@@ -0,0 +1,112 @@
+require("dotenv").config()
+const Web3 = require('web3');
+const { ChainId, Token, TokenAmount, Pair } = require('@uniswap/sdk');
+const abis = require('./abis');
+const { mainnet: addresses } = require('./addresses');
+
+const web3 = new Web3(
+  new Web3.providers.WebsocketProvider(process.env.INFURA_URL)
+);
+
+const kyber = new web3.eth.Contract(
+  abis.kyber.kyberNetworkProxy,
+  addresses.kyber.kyberNetworkProxy
+);
+
+const AMOUNT_ETH = 100;
+const RECENT_ETH_PRICE = 230;
+const AMOUNT_ETH_WEI = web3.utils.toWei(AMOUNT_ETH.toString());
+const AMOUNT_DAI_WEI = web3.utils.toWei((AMOUNT_ETH * RECENT_ETH_PRICE).toString());
+
+const init = async () => {
+  const WETH = new Token(
+    ChainId.MAINNET, 
+    addresses.tokens.weth, 
+    18, 
+    'WETH', 
+    'Wrapped Ether'
+  );
+  const DAI = new Token(
+    ChainId.MAINNET, 
+    addresses.tokens.dai, 
+    18, 
+    'DAI', 
+    'Dai Stablecoin'
+  );
+  const [dai, weth] = await Promise.all(
+    [addresses.tokens.dai, addresses.tokens.weth].map(tokenAddress => (
+      Token.fetchData(
+        ChainId.MAINNET,
+        tokenAddress,
+      )
+  )));
+  daiWeth = await Pair.fetchData(
+    dai,
+    weth,
+  );
+
+  web3.eth.subscribe('newBlockHeaders')
+    .on('data', async block => {
+      console.log(`New block received. Block # ${block.number}`);
+
+      const kyberResults = await Promise.all([
+          kyber
+            .methods
+            .getExpectedRate(
+              addresses.tokens.dai, 
+              '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 
+              AMOUNT_DAI_WEI
+            ) 
+            .call(),
+          kyber
+            .methods
+            .getExpectedRate(
+              '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 
+              addresses.tokens.dai, 
+              AMOUNT_ETH_WEI
+            ) 
+            .call()
+      ]);
+      const kyberRates = {
+        buy: parseFloat(1 / (kyberResults[0].expectedRate / (10 ** 18))),
+        sell: parseFloat(kyberResults[1].expectedRate / (10 ** 18))
+      };
+      console.log('Kyber ETH/DAI');
+      console.log(kyberRates);
+
+      const uniswapResults = await Promise.all([
+        daiWeth.getOutputAmount(new TokenAmount(DAI, AMOUNT_DAI_WEI)),
+        daiWeth.getOutputAmount(new TokenAmount(WETH, AMOUNT_ETH_WEI))
+      ]);
+      const uniswapRates = {
+        buy: parseFloat( AMOUNT_DAI_WEI / (uniswapResults[0][0].toExact() * 10 ** 18)),
+        sell: parseFloat(uniswapResults[1][0].toExact() / AMOUNT_ETH),
+      };
+      console.log('Uniswap ETH/DAI');
+      console.log(uniswapRates);
+
+      const gasPrice = await web3.eth.getGasPrice();
+      //200000 is picked arbitrarily, have to be replaced by actual tx cost in next lectures, with Web3 estimateGas()
+      const txCost = 200000 * parseInt(gasPrice);
+      const currentEthPrice = (uniswapRates.buy + uniswapRates.sell) / 2; 
+      const profit1 = (parseInt(AMOUNT_ETH_WEI) / 10 ** 18) * (kyberRates.sell - uniswapRates.buy) - (txCost / 10 ** 18) * currentEthPrice;
+      const profit2 = (parseInt(AMOUNT_ETH_WEI) / 10 ** 18) * (uniswapRates.sell - kyberRates.buy) - (txCost / 10 ** 18) * currentEthPrice;
+      if(profit1 > 0) {
+        console.log('Arb opportunity found!');
+        console.log(`Buy ETH from Uniswap at ${uniswapRates.buy} dai`);
+        console.log(`Sell ETH from Kyber at ${kyberRates.sell} dai`);
+        console.log(`Expected profit: ${profit1} dai`);
+        //execute arb
+      } else if(profit2 > 0) {
+        console.log('Arb opportunity found!');
+        console.log(`Buy ETH from Kyber at ${kyberRates.buy} dai`);
+        console.log(`Sell ETH from Uniswap at ${uniswapRates.sell} dai`);
+        console.log(`Expected profit: ${profit2} dai`);
+      }
+    })
+    .on('error', error => {
+      console.log(error);
+    });
+}
+init();
+
