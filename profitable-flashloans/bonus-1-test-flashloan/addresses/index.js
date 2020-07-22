7234b16e2f5cf7581d4e7abaf4f90d280c950291
Julien Klepatch <julien@julienklepatch.com>
2020-07-23 03:25:21 +0800

    add kovan addresses

--- profitable-flashloans/bonus-1-test-flashloan/addresses/index.js
+++ profitable-flashloans/bonus-1-test-flashloan/addresses/index.js
@@ -4,6 +4,12 @@ const dydxMainnet = require('./dydx-mainnet.json');
 const tokensMainnet = require('./tokens-mainnet.json');
 const makerdaoMainnet = require('./makerdao-mainnet.json');
 
+const kyberKovan = require('./kyber-kovan.json');
+const uniswapKovan = require('./uniswap-kovan.json');
+const dydxKovan = require('./dydx-kovan.json');
+const tokensKovan = require('./tokens-kovan.json');
+const makerdaoKovan = require('./makerdao-kovan.json');
+
 module.exports = {
   mainnet: {
     kyber: kyberMainnet,
@@ -11,5 +17,12 @@ module.exports = {
     dydx: dydxMainnet,
     tokens: tokensMainnet,
     makerdao: makerdaoMainnet
+  },
+  kovan: {
+    kyber: kyberKovan,
+    uniswap: uniswapKovan,
+    dydx: dydxKovan,
+    tokens: tokensKovan,
+    makerdao: makerdaoKovan
   }
 };
