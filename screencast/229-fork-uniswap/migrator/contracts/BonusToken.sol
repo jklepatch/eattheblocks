0769f47ae0e8d65508ec18a45f7bc26d47f4f913
Julien Klepatch <julien@julienklepatch.com>
2020-09-17 23:58:29 +0800

    update migrator

--- screencast/229-fork-uniswap/migrator/contracts/BonusToken.sol
+++ screencast/229-fork-uniswap/migrator/contracts/BonusToken.sol
@@ -1,4 +1,21 @@
 pragma solidity =0.6.6;
 
-contract MyToken {
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract MyToken is ERC20 {
+  address public admin;
+  address public liquidator;
+  constructor() ERC20('Bonus Token', 'BTK') public {
+    admin = msg.sender;
+  }
+
+  function setLiquidator(address _liquidator) external {
+    require(msg.sender == admin, 'only admin');
+    liquidator = _liquidator;
+  }
+
+  function giveBonus(address to, uint amount) external {
+    require(msg.sender == liquidator, 'only liquidator');
+    _mint(to, amount); 
+  }
 }
--- screencast/229-fork-uniswap/migrator/contracts/LiquidityMigrator.sol
+++ screencast/229-fork-uniswap/migrator/contracts/LiquidityMigrator.sol
@@ -3,12 +3,14 @@ pragma solidity =0.6.6;
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
 import './IUniswapV2Pair.sol';
+import './BonusToken.sol';
 
 contract LiquidityMigrator {
   IUniswapV2Router02 public router;
   IUniswapV2Pair public pair;
   IUniswapV2Router02 public routerFork;
   IUniswapV2Pair public pairFork;
+  BonusToken public bonusToken;
   address public admin;
   mapping(address => uint) public unclaimedBalances;
   bool public migrationDone;
@@ -17,18 +19,21 @@ contract LiquidityMigrator {
     address _router, 
     address _pair, 
     address _routerFork,
-    address _pairFork
+    address _pairFork,
+    address _bonusToken
   ) public {
     router = IUniswapV2Router02(_router);
     pair = IUniswapV2Pair(_pair); 
     routerFork = IUniswapV2Router02(_routerFork);
     pairFork = IUniswapV2Pair(_pairFork);
+    bonusToken = IERC20(_bonusToken);
     admin = msg.sender;
   }
 
   function deposit(uint amount) external {
     require(migrationDone == false, 'migration already done');
     pair.transferFrom(msg.sender, address(this), amount);
+    bonusToken.mint(msg.sender, amount);
     unclaimedBalances[msg.sender] += amount;
   }
 
