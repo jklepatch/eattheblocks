cb36da4744e8b5e13f841e5b8bd4fceacb4fee4b
Julien Klepatch <julien@julienklepatch.com>
2020-12-08 23:13:49 +0800

    add new lectures to defi course

--- defi-development-mastery/1-defi-building-blocks/5-wrapped-ether/contracts/Weth.sol
+++ defi-development-mastery/1-defi-building-blocks/5-wrapped-ether/contracts/Weth.sol
@@ -5,8 +5,8 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 contract Weth is ERC20 {
   constructor() ERC20('Wrapped Ether', 'WETH') {}
 
-  function deposit(address to, uint amount) external payable {
-    _mint(to, amount);
+  function deposit() external payable {
+    _mint(msg.sender, msg.value);
   }
 
   function withdraw(uint amount) external {
---
+++ defi-development-mastery/1-defi-building-blocks/6-collateral-backed-tokens/contracts/CollateralBackedToken.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.7.3;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract CollateralBackedToken is ERC20 {
+  IERC20 public collateral;
+  uint price = 1;
+  constructor(address _collateral) ERC20('Collateral Backed Token', 'CBT') {
+    collateral = IERC20(_collateral);
+  }
+
+  function deposit(uint collateralAmount) external {
+    collateral.transferFrom(msg.sender, address(this), collateralAmount);
+    _mint(msg.sender, collateralAmount * price);
+  }
+
+  function withdraw(uint tokenAmount) external {
+    require(balanceOf(msg.sender) >= tokenAmount, 'balance too low');
+    _burn(msg.sender, tokenAmount);
+    msg.sender.transfer(tokenAmount / price);
+  }
+}
