7b56dddf80649b4291eed6ed79c6f752ea865fde
Julien Klepatch <julien@julienklepatch.com>
2021-01-20 01:02:34 +0800

    update yield farming

--- defi-development-mastery/1-defi-building-blocks/11-yield-farming/contracts/YieldFarmer.sol
+++ defi-development-mastery/1-defi-building-blocks/11-yield-farming/contracts/YieldFarmer.sol
@@ -1,17 +1,54 @@
-pragma solidity ^0.7.2;
+pragma solidity ^0.7.3;
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 contract YieldFarmer {
-  function rebalance() external {
-    uint yieldA = pricer.getYieldA();
-    uint yieldB = pricer.getYieldB();
-    uint yieldC = pricer.getYieldC();
+  enum Protocol { A, B, C }
+  Protocol public currentProtocol;
+  IERC20 public token;
+
+  constructor(address _token) {
+    token = IERC20(_token);
+  }
+
+  function deposit(uint amount) external {
+    token.transferFrom(msg.sender, address(this), amount);
+    withdrawFrom(currentProtocol);
+    _rebalance();
+  }
+
+  function withdraw(uint amount) external {
+    withdrawFrom(currentProtocol);
+    token.transfer(msg.sender, amount);
+    _rebalance();
+  }
+
+  function rebalance() public {
+    withdrawFrom(currentProtocol);
+    _rebalance();
+  }
+
+  function _rebalance() internal {
+    uint yieldA = getYieldA();
+    uint yieldB = getYieldB();
+    uint yieldC = getYieldC();
+
     if(yieldA >= yieldB && yieldA >= yieldC) {
-      
+      investTo(Protocol.A);
+      currentProtocol = Protocol.A;
+    } else if(yieldB >= yieldA && yieldB >= yieldC) {
+      investTo(Protocol.B);
+      currentProtocol = Protocol.B;
+    } else if(yieldC >= yieldA && yieldC >= yieldB) {
+      investTo(Protocol.C);
+      currentProtocol = Protocol.C;
     }
-    if(yieldB >= yieldA && yieldB >= yieldC) {
-    }
-    if(yieldC >= yieldA && yieldC >= yieldB) {
-    }
+  }
+
+  function withdrawFrom(Protocol protocol) internal {
+  }
+
+  function investTo(Protocol protocol) internal {
   }
 
   function getYieldA() public view returns(uint) {
