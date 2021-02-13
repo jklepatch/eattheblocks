b8df083fa53518293ce3a7f434c5ce7a3d7f8792
Julien Klepatch <julien@julienklepatch.com>
2021-02-13 17:26:47 +0800

    typo

--- screencast/298-arbitrage-uniswap-sushiswap/contracts/Arbitrage.sol
+++ screencast/298-arbitrage-uniswap-sushiswap/contracts/Arbitrage.sol
@@ -70,7 +70,7 @@ contract Arbitrage {
       deadline
     )[1];
 
-    token.transfer(msg.sender, amountRequired));
+    token.transfer(msg.sender, amountRequired);
     token.transfer(tx.origin, amountReceived - amountRequired);
   }
 }
