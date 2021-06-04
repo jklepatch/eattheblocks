a34d87ad88245328be7faa888954850c3c9dcfc2
Julien Klepatch <julien@julienklepatch.com>
2021-06-05 03:54:19 +0800

    add charity fee

--- live-trainings/1-fork-safemoon/contracts/Token.sol
+++ live-trainings/1-fork-safemoon/contracts/Token.sol
@@ -5,7 +5,9 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 contract Token is ERC20 {
   uint BURN_TAX = 5;
   uint ADMIN_TAX = 5;
+  uint CHARITY_TAX = 5;
   address public admin;
+  address public charity;
   mapping(address => bool) public excludedFromTax;
 
   constructor() public ERC20('TOKEN', 'TKN') {
@@ -23,9 +25,15 @@ contract Token is ERC20 {
     } else { 
       uint burnAmount = amount.mul(BURN_TAX) / 100;
       uint adminAmount = amount.mul(ADMIN_TAX) / 100;
+      uint charityAmount = amount.mul(CHARITY_TAX) / 100;
       _burn(_msgSender(), burnAmount);
       _transfer(_msgSender(), admin, adminAmount);
-      _transfer(_msgSender(), recipient, amount.sub(burnAmount).sub(adminAmount));
+      _transfer(_msgSender(), charity, charityAmount);
+      uint recipientAmount = amount
+        .sub(burnAmount)
+        .sub(adminAmount)
+        .sub(charityAmount);
+      _transfer(_msgSender(), recipient, recipientAmount); 
     }
     return true;
   }
@@ -47,16 +55,19 @@ contract Token is ERC20 {
     } else {
       uint burnAmount = amount.mul(BURN_TAX) / 100;
       uint adminAmount = amount.mul(ADMIN_TAX) / 100;
+      uint charityAmount = amount.mul(CHARITY_TAX) / 100;
       _burn(sender, burnAmount);
       _transfer(sender, admin, adminAmount);
-      _transfer(sender, recipient, amount.sub(burnAmount).sub(adminAmount));
+      _transfer(sender, charity, charityAmount);
+      uint recipientAmount = amount
+        .sub(burnAmount)
+        .sub(adminAmount)
+        .sub(charityAmount);
+      _transfer(sender, recipient, recipientAmount); 
       _approve(
         sender, 
         _msgSender(),
-        allowance(sender, _msgSender()).sub(
-          amount.sub(burnAmount).sub(adminAmount), 
-          "ERC20: transfer amount exceeds allowance"
-        )
+        allowance(sender, _msgSender()).sub(recipientAmount)
       );
     }
     return true;
@@ -65,5 +76,10 @@ contract Token is ERC20 {
   function addExcludedFromTax(address excluded) external {
     require(msg.sender == admin, 'only admin');
     excludedFromTax[excluded] = true;
+  }
+
+  function updateCharity(address _charity) external {
+    require(msg.sender == admin, 'only admin');
+    charity = _charity;
   }
 }
