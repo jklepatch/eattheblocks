18fd902940804e95eebeebd9423201d1f2345aa7
WarMachineAi <74069358+WarMachineAi@users.noreply.github.com>
2021-10-16 03:00:37 +0400

    Update 2_deploy_contracts.js
    pancakeswap v2 addresses

--- profitable-flashloans/update/28-depoly-flashloan-smart-contract/migrations/2_deploy_contracts.js
+++ profitable-flashloans/update/28-depoly-flashloan-smart-contract/migrations/2_deploy_contracts.js
@@ -3,8 +3,8 @@ const flashloan = artifacts.require("Flashloan.sol");
 module.exports = function (deployer) {
   deployer.deploy(
     flashloan,
-    '0xBCfCcbde45cE874adCB698cC183deBcF17952812', //PancakeSwap factory
+    '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73', //PancakeSwap factory
     '0xCDe540d7eAFE93aC5fE6233Bee57E1270D3E330F', //BakerySwap router
-    '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'  //PancakeSwap router 
+    '0x10ED43C718714eb63d5aA57B78B54704E256024E'  //PancakeSwap router 
     );
 };
