c58ff2279d4c524dfe572d201b93c3afc43eb240
TimothyHyland <moldymilk@hotmail.com>
2022-07-31 11:48:04 -0400

    Refactor to simplify

--- profitable-flashloans/bsc-flashswap-update/28-deploy-flashloan-smart-contract/addresses/index.js
+++ profitable-flashloans/bsc-flashswap-update/28-deploy-flashloan-smart-contract/addresses/index.js
@@ -1,20 +1,20 @@
-const pancakeSwapMainnet = require('./pancakeSwap-mainnet.json');
-const apeSwapMainnet = require('./apeSwap-mainnet.json');
-const tokensMainnet = require('./tokens-mainnet.json');
+const pancakeSwapMainnet = require("./pancakeSwap-mainnet.json");
+const apeSwapMainnet = require("./apeSwap-mainnet.json");
+const tokensMainnet = require("./tokens-mainnet.json");
 
-const pancakeSwapTestnet = require('./pancakeSwap-testnet.json');
-const apeSwapTestnet = require('./apeSwap-testnet.json');
-const tokensTestnet = require('./tokens-testnet.json');
+//const pancakeSwapTestnet = require('./pancakeSwap-testnet.json');
+//const apeSwapTestnet = require('./apeSwap-testnet.json');
+//const tokensTestnet = require('./tokens-testnet.json');
 
 module.exports = {
   mainnet: {
     pancakeSwap: pancakeSwapMainnet,
     apeSwap: apeSwapMainnet,
-    tokens: tokensMainnet
+    tokens: tokensMainnet,
   },
-  testnet: {
-    pancakeSwap: pancakeSwapTestnet,
-    apeSwap: apeSwapTestnet,
-    tokens: tokensTestnet
-  }
+  //testnet: {
+  //  pancakeSwap: pancakeSwapTestnet,
+  //  apeSwap: apeSwapTestnet,
+  //  tokens: tokensTestnet
+  //}
 };
--- profitable-flashloans/bsc-flashswap-update/28-deploy-flashloan-smart-contract/run-arbitrage.js
+++ profitable-flashloans/bsc-flashswap-update/28-deploy-flashloan-smart-contract/run-arbitrage.js
@@ -3,6 +3,9 @@ const Web3 = require("web3");
 const abis = require("./abis");
 const { mainnet: addresses } = require("./addresses");
 // const { testnet: addresses } = require("./addresses");  added for testing on testnet
+
+//   *************** To run code without having contract deployed   Comment out line 9 and 33 to 37   *************************
+
 const Flashloan = require("./build/contracts/FlashSwap.json");
 
 const web3 = new Web3(
@@ -29,6 +32,7 @@ const PancakeSwap = new web3.eth.Contract(
 
 const init = async () => {
   const networkId = await web3.eth.net.getId();
+  //*************** To run code without having contract deployed   Comment out line 9 and 33 to 37   *************************
   const flashloan = new web3.eth.Contract(
     Flashloan.abi,
     Flashloan.networks[networkId].address
@@ -40,86 +44,86 @@ const init = async () => {
       console.log(`New block received. Block # ${block.number}`);
 
       const amountsOut1 = await ApeSwap.methods
-      .getAmountsIn(amountInBUSD, [
-        addresses.tokens.WBNB,
-        addresses.tokens.BUSD,
-      ])
-      .call();
-    const amountsOut2 = await ApeSwap.methods
-      .getAmountsOut(amountInBUSD, [
-        addresses.tokens.BUSD,
-        addresses.tokens.WBNB,
-      ])
-      .call();
+        .getAmountsIn(amountInBUSD, [
+          addresses.tokens.WBNB,
+          addresses.tokens.BUSD,
+        ])
+        .call();
+      const amountsOut2 = await ApeSwap.methods
+        .getAmountsOut(amountInBUSD, [
+          addresses.tokens.BUSD,
+          addresses.tokens.WBNB,
+        ])
+        .call();
 
-    const amountsOut3 = await PancakeSwap.methods
-      .getAmountsIn(amountInBUSD, [
-        addresses.tokens.WBNB,
-        addresses.tokens.BUSD,
-      ])
-      .call();
-    const amountsOut4 = await PancakeSwap.methods
-      .getAmountsOut(amountInBUSD, [
-        addresses.tokens.BUSD,
-        addresses.tokens.WBNB,
-      ])
-      .call();
+      const amountsOut3 = await PancakeSwap.methods
+        .getAmountsIn(amountInBUSD, [
+          addresses.tokens.WBNB,
+          addresses.tokens.BUSD,
+        ])
+        .call();
+      const amountsOut4 = await PancakeSwap.methods
+        .getAmountsOut(amountInBUSD, [
+          addresses.tokens.BUSD,
+          addresses.tokens.WBNB,
+        ])
+        .call();
 
-    const amountsOut5 = await ApeSwap.methods
-      .getAmountsIn(amountInWBNB, [
-        addresses.tokens.BUSD,
-        addresses.tokens.WBNB,
-      ])
-      .call();
-    const amountsOut6 = await ApeSwap.methods
-      .getAmountsOut(amountInWBNB, [
-        addresses.tokens.WBNB,
-        addresses.tokens.BUSD,
-      ])
-      .call();
+      const amountsOut5 = await ApeSwap.methods
+        .getAmountsIn(amountInWBNB, [
+          addresses.tokens.BUSD,
+          addresses.tokens.WBNB,
+        ])
+        .call();
+      const amountsOut6 = await ApeSwap.methods
+        .getAmountsOut(amountInWBNB, [
+          addresses.tokens.WBNB,
+          addresses.tokens.BUSD,
+        ])
+        .call();
 
-    const amountsOut7 = await PancakeSwap.methods
-      .getAmountsIn(amountInWBNB, [
-        addresses.tokens.BUSD,
-        addresses.tokens.WBNB,
-      ])
-      .call();
-    const amountsOut8 = await PancakeSwap.methods
-      .getAmountsOut(amountInWBNB, [
-        addresses.tokens.WBNB,
-        addresses.tokens.BUSD,
-      ])
-      .call();
+      const amountsOut7 = await PancakeSwap.methods
+        .getAmountsIn(amountInWBNB, [
+          addresses.tokens.BUSD,
+          addresses.tokens.WBNB,
+        ])
+        .call();
+      const amountsOut8 = await PancakeSwap.methods
+        .getAmountsOut(amountInWBNB, [
+          addresses.tokens.WBNB,
+          addresses.tokens.BUSD,
+        ])
+        .call();
 
-    const aperesults = {
-      buy: amountsOut1[0] / 10 ** 18,
-      sell: amountsOut2[1] / 10 ** 18,
-    };
-    const aperesults2 = {
-      buy: amountsOut5[0] / 10 ** 18,
-      sell: amountsOut6[1] / 10 ** 18,
-    };
+      const aperesults = {
+        buy: amountsOut1[0] / 10 ** 18,
+        sell: amountsOut2[1] / 10 ** 18,
+      };
+      const aperesults2 = {
+        buy: amountsOut5[0] / 10 ** 18,
+        sell: amountsOut6[1] / 10 ** 18,
+      };
 
-    const pancakeresults = {
-      buy: amountsOut3[0] / 10 ** 18,
-      sell: amountsOut4[1] / 10 ** 18,
-    };
-    const pancakeresults2 = {
-      buy: amountsOut7[0] / 10 ** 18,
-      sell: amountsOut8[1] / 10 ** 18,
-    };
+      const pancakeresults = {
+        buy: amountsOut3[0] / 10 ** 18,
+        sell: amountsOut4[1] / 10 ** 18,
+      };
+      const pancakeresults2 = {
+        buy: amountsOut7[0] / 10 ** 18,
+        sell: amountsOut8[1] / 10 ** 18,
+      };
 
-    console.log(`ApeSwap ${flashloanBUSD} BUSD/WBNB `);
-    console.log(aperesults);
+      console.log(`ApeSwap ${flashloanBUSD} BUSD/WBNB `);
+      console.log(aperesults);
 
-    console.log(`PancakeSwap ${flashloanBUSD} BUSD/WBNB`);
-    console.log(pancakeresults);
+      console.log(`PancakeSwap ${flashloanBUSD} BUSD/WBNB`);
+      console.log(pancakeresults);
 
-    console.log(`ApeSwap ${flashloanWBNB} WBNB/BUSD`);
-    console.log(aperesults2);
+      console.log(`ApeSwap ${flashloanWBNB} WBNB/BUSD`);
+      console.log(aperesults2);
 
-    console.log(`PancakeSwap${flashloanWBNB} WBNB/BUSD `);
-    console.log(pancakeresults2);
+      console.log(`PancakeSwap${flashloanWBNB} WBNB/BUSD `);
+      console.log(pancakeresults2);
 
       //Payback fee calc
 
